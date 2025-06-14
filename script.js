12131afbe2d2743e2f1652885f56180ce1eac012
Gero Ferrari <gerooferrari@gmail.com>
2025-06-14 19:00:38 -0300

    update galery and prices

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 })
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+  }
+
+  // Mostrar el popup solo una vez por sesión
+  window.onload = function () {
+    if (!sessionStorage.getItem('popupShown')) {
+      document.getElementById('popup').style.display = 'block';
+      sessionStorage.setItem('popupShown', 'true');
+    } else {
+      document.getElementById('popup').style.display = 'none';
+    }
+  };
